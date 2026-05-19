0a47a4db6cd3d30e65c54285c1cbee795d143d92
Yanis Dumas <yanisdumas@MacBook-Air-de-Yanis-2.local>
2026-05-19 14:58:27 +0200

    Déplacement de la connexion à GitHub des paramètres au script de run

--- run_all.R
+++ run_all.R
@@ -4,6 +4,30 @@
 #        Sourcer ce fichier suffit à enchaîner les modules choisis.
 ################################################################################
 
+################################################################################
+# ── POUR RETROUVER LE DÉPÔT GITHUB ────────────────────────────────────────────
+#  system("git clone https://github.com/Yanis2001/Fret_Rwanda_AFD.git")
+################################################################################
+
+# ==============================================================================
+# CONNEXION GIT
+# ==============================================================================
+
+# Authentification Git via le Personal Access Token stocké en variable d'env.
+# Sys.getenv() lit la variable d'environnement GITHUB_PAT sans l'exposer
+# dans le code source (bonne pratique de sécurité).
+token <- Sys.getenv("GITHUB_PAT")
+# Configurer le helper de credentials : plus besoin de mettre le mot de passe et nom d'utilisateur avant de pusher sur Git
+system("git config --global credential.helper '!f() { echo \"username=token\"; echo \"password=$GITHUB_PAT\"; }; f'")
+
+# S'assurer que le remote 'origin' pointe vers mon dépôt perso
+system("git remote set-url origin https://github.com/Yanis2001/Fret_Rwanda_AFD.git")
+# Pusher le script sur deux Git
+system("git remote set-url --add --push origin https://github.com/Yanis2001/Fret_Rwanda_AFD.git")
+system("git remote set-url --add --push origin https://github.com/GEMMES-AFD/Transport.git")
+# Vérifier la configuration
+system("git remote -v")
+
 # ==============================================================================
 # CONFIGURATION DU RUN
 # Mettre TRUE pour les modules à exécuter, FALSE pour les sauter.
